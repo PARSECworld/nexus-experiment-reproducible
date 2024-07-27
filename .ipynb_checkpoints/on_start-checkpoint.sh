92657016a337a7c573dcbe11f2fcd74141f7ca80
leoczzi <leoczzi@yahoo.com>
2024-07-27 05:08:45 +0000

    teste

--- .ipynb_checkpoints/on_start-checkpoint.sh
+++ .ipynb_checkpoints/on_start-checkpoint.sh
@@ -4,4 +4,15 @@ apt-get install -y graphviz libgraphviz-dev pkg-config libffi-dev
 . /home/sagemaker-user/reproducible/nexus_experiment/bin/activate
 python -m ipykernel install --user --name nexus_experiment --display-name "Nexus Experiment"
 apt install git
+# Verificar e configurar credenciais Git se necessário
+git remote set-url origin https://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@github.com/PARSECworld/nexus-reproducible.git
+
+#git remote set-url origin https://leoczzi:dfxJPiLkqOpg3IFv3MJOWSK44Uik9m1GikLL@github.com/leoczzi/nexus-reproducible.git
+
+# Se não estiverem configurados, configurar o user e email
+git config --global user.name "leoczzi"
+git config --global user.email "leoczzi@yahoo.com"
+
+git push --set-upstream origin develop
+
 deactivate
